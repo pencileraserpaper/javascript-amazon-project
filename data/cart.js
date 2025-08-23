3217b42e158d0bc955336557d6789fb7e0c85c67
Your Name <224414882+pencileraserpaper@users.noreply.github.com>
2025-08-23 17:38:29 +0530

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,19 @@
 export const cart = [];
+
+export function addToCart(productId){
+  let matchingItem; // check if the item already exist in the cart, if so, increase quantity by 1
+  cart.forEach((cartItem) => {
+    if(productId === cartItem.productId){
+      matchingItem = cartItem;
+    }
+  });
+
+  if(matchingItem){
+    matchingItem.quantity += 1;
+  }else{
+    cart.push({
+      productId,
+      quantity: 1
+    });
+  }
+}
