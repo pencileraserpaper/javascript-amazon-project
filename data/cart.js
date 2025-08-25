7e8e5d286c7ec705a79964ea5ccc9d588e525ec6
Your Name <224414882+pencileraserpaper@users.noreply.github.com>
2025-08-25 09:31:59 +0530

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart')) || [
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
@@ -8,6 +8,10 @@ export let cart = [
     quantity: 1
   }
 ];
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId){
   let matchingItem; // check if the item already exist in the cart, if so, increase quantity by 1
@@ -25,6 +29,8 @@ export function addToCart(productId){
       quantity: 1
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -37,4 +43,6 @@ export function removeFromCart(productId){
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
